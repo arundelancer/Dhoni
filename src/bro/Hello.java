4c0cb0fff79fe12cb51db1b4d1e2e1688149db62
arundelancer <arundelancer@gmail.com>
2019-12-13 21:20:46 +0530

    i have done my test cases

---
+++ src/bro/Hello.java
@@ -0,0 +1,30 @@
+package bro;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Hello {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\lenovo\\eclipse-workspace\\browser\\dev\\chromedriver.exe");
+		WebDriver d=new ChromeDriver();
+		d.get("https://www.toolsqa.com/automation-practice-table/");
+		List<WebElement> b = d.findElements(By.tagName("tr"));
+	
+			for (int i = 4; i<6; i++) {
+				WebElement h = b.get(i);
+				String text2 = h.getText();
+				System.out.println(text2);
+			 
+			
+			}
+		}
+		
+	
+	}
+
+
